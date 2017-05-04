b828bd559982f47bb54b6f56b1ab241db856f406
gonzaabel <gonzaloabel@hotmail.com>
2017-05-04 20:52:30 -0300

    Small fix for alevilar/paxapos-plugin#116 - assing SuperRol to user now work correctly

--- Model/SuperRolUser.php
+++ Model/SuperRolUser.php
@@ -19,7 +19,7 @@ class SuperRolUser extends RistoAppModel {
     public function asignarRol($data) {
     	$this->deleteAll(array('user_id' => $data['User']['id']), false);
     	$indice = 0;
-    	foreach($data['Rol']['Rol'] as $r) {
+    	foreach($data['SuperRol']['SuperRol'] as $r) {
 
     	   $rolesUser[$indice] = array('user_id' => $data['User']['id'], 'rol_id' => $r);    	     	
 
